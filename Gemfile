11aa190969578059281d03ee0981f74a7aa3b4c5
thinkphp <mergesortv@gmail.com>
2021-04-29 23:50:29 +0300

    first commit Sinatra

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'http://rubygems.org'
+ruby '2.7.1'
+gem 'sinatra'
---
+++ app.rb
@@ -0,0 +1,34 @@
+require 'sinatra'
+require 'yaml/store'
+
+Choices = {
+  'HAM' => 'Hamburger',
+  'PIZ' => 'Pizza',
+  'CUR' => 'Curry',
+  'NOO' => 'Noodles',
+}
+
+get '/' do
+   @title = 'Welcome to the Jungle!'
+   erb :index
+end
+
+post '/action' do
+  @title = 'You are welcome!'
+  @vote  = params['vote']
+  @store = YAML::Store.new 'votes.yml'
+  @store.transaction do
+    @store['votes'] ||= {}
+    @store['votes'][@vote] ||= 0
+    @store['votes'][@vote] += 1
+  end
+  erb :cast
+end
+
+
+get '/results' do
+  @title = 'Results so far:'
+  @store = YAML::Store.new 'votes.yml'
+  @votes = @store.transaction { @store['votes'] }
+  erb :results
+end
